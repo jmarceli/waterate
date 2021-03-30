ea5338bdf30501975f434edea9c3669710364301
Jan Grzegorowski <grzegorowski@gmail.com>
2021-03-30 20:22:48 +0200

    Fix perf issues

--- src/shared/db/addWaterNow.ts
+++ src/shared/db/addWaterNow.ts
@@ -4,30 +4,26 @@ import { incrementStats } from './updateStats';
 
 // TODO: maybe wrap with some throttling function instead of retrying
 export const addWaterNow = async (quantity: number): Promise<void> => {
-  let success = false;
-  while (!success) {
+  try {
+    const isoToday = await addWaterAtSecond(quantity);
     try {
-      const isoToday = await addWaterAtSecond(quantity);
-      try {
-        await incrementStats(quantity.toFixed(0));
-      } catch (error) {
-        console.error(error);
-      }
-      try {
-        await recalculateDaily(isoToday);
-      } catch (error) {
-        console.error(error);
-      }
-      success = true;
-      // console.log('success', success);
-    } catch (err) {
-      // retry if conflict happens
-      if (err.name !== 'conflict') {
-        console.error(err);
-        throw err;
-      }
+      await incrementStats(quantity.toFixed(0));
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      await recalculateDaily(isoToday);
+    } catch (error) {
+      console.error(error);
+    }
+  } catch (err) {
+    // retry if conflict happens
+    if (err.name !== 'conflict') {
+      console.error(err);
+      throw err;
     }
   }
+  console.log('addWater end');
 };
 
 export const recalculateDaily = async (isoToday: Dayjs): Promise<void> => {
@@ -73,12 +69,19 @@ const updateWaterAtDay = async (id: string, quantity: number) => {
   return res;
 };
 
+// counter is used as ms value to ensure uniqueness in case of fast clicks
+// while this is not an ideal solution it works, the target solution would be writes batching
+let counter: number = 0;
+
 const addWaterAtSecond = async (quantity: number): Promise<Dayjs> => {
   const isoDateTime = dayjs();
   await db.put({
     quantity: quantity,
-    _id: isoDateTime.format('YYYY-MM-DDTHH:mm:ss'),
+    _id:
+      isoDateTime.format('YYYY-MM-DDTHH:mm:ss.') +
+      counter.toString().padStart(3, '0'),
   });
+  counter = (counter + 1) % 1000;
   return isoDateTime;
 };
 
